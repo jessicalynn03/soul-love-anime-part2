dd941e5c72c91cdc189868c7457802197e86aeee
jessicalynn03 <jessica.barthelemy03@gmail.com>
2022-05-09 07:04:50 -0400

    functionality added to blog page

--- src/components/AnimeShow.js
+++ src/components/AnimeShow.js
@@ -1,11 +1,34 @@
-const AnimeShow = (props) =>{
-  console.log(props);
+import {useState} from 'react';
+const AnimeShow = (props) => {
+    console.log(props);
+    const [postComment, setPostComment] = useState([]);
+    const [comment, setComment] = useState("");
 
 
-    return(
-        <div>
-            <h1>Anime Show Page</h1>
-        </div>
-    )
-}
-export default AnimeShow;
+    const postSubmit = (e) => {
+        e.preventDefault()
+        setPostComment([...postComment, comment])
+    }
+        return (
+            <div>
+                <form onSubmit={postSubmit}>
+                    {/* <input className="comment-input" type="text" value={comment} onChange={(e) => {setComment(e.target.value)}}/> */}
+                    <textarea type='text' value={comment} onChange={(e) => { setComment(e.target.value) }} rows='5' cols='80' id='blog-post'></textarea>
+                    <hr></hr>
+                    <input type="submit" value="Create Post" />
+                </form>
+                <ul className="comment-post">
+                    {postComment.map((item, index) => {
+                        return (
+                            <li key={index}>
+
+                                {item}</li>
+                        )
+                    })
+
+                    }
+                </ul>
+            </div>
+        )
+    }
+    export default AnimeShow;
